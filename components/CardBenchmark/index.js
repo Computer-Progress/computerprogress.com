1c41fed3a25e184871f45fbc315568388c4f8597
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-16 11:24:41 -0300

    feat(component): hide benchmark data on mobile

--- components/CardBenchmark/index.js
+++ components/CardBenchmark/index.js
@@ -1,43 +1,48 @@
+import { useTheme } from "@material-ui/core/styles";
+import { useMediaQuery } from "@material-ui/core";
 import { StyledBox, FlexBox, FlexItem, StyledDivider } from "./style";
 
 export default function CardBenchmark(props) {
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("md"));
+
   return (
-    <StyledBox boxShadow={1} borderRadius={10} px={3} py={1}>
+    <StyledBox px={3} py={1}>
       <h3>{props.benchmark.title}</h3>
 
-      <StyledDivider />
+      {isLargeScreen && (
+        <>
+          <StyledDivider />
 
-      <FlexBox>
-        <FlexItem>
-          <p>Best model</p>
+          <FlexBox>
+            <FlexItem>
+              <p>Best model</p>
 
-          <h4>{props.benchmark.bestModel}</h4>
-        </FlexItem>
+              <h4>{props.benchmark.bestModel}</h4>
+            </FlexItem>
+            <FlexItem pl={2}>
+              <p>Paper</p>
 
-        <FlexItem pl={2}>
-          <p>Paper</p>
+              <h4>{props.benchmark.paper}</h4>
+            </FlexItem>
+            <FlexItem pl={2} textAlign="center">
+              <p>BLEU</p>
 
-          <h4>{props.benchmark.paper}</h4>
-        </FlexItem>
+              <h4>{props.benchmark.BLEU}%</h4>
+            </FlexItem>
+            <FlexItem pl={2} textAlign="right">
+              <p>Hardware Burden</p>
 
-        <FlexItem pl={2} textAlign="center">
-          <p>BLEU</p>
+              <h4>{props.benchmark.hardwareBurden}</h4>
+            </FlexItem>
+            <FlexItem pl={2} textAlign="right">
+              <p>Publication date</p>
 
-          <h4>{props.benchmark.BLEU}%</h4>
-        </FlexItem>
-
-        <FlexItem pl={2} textAlign="right">
-          <p>Hardware Burden</p>
-
-          <h4>{props.benchmark.hardwareBurden}</h4>
-        </FlexItem>
-
-        <FlexItem pl={2} textAlign="right">
-          <p>Publication date</p>
-
-          <h4>{props.benchmark.publicationDate}</h4>
-        </FlexItem>
-      </FlexBox>
+              <h4>{props.benchmark.publicationDate}</h4>
+            </FlexItem>
+          </FlexBox>
+        </>
+      )}
     </StyledBox>
   );
 }
