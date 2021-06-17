6909d961e5115cb73eca234e580b5a39beac258d
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-17 20:17:41 -0300

    feat(component): make the description card a component

---
+++ components/DescriptionCard/index.js
@@ -0,0 +1,25 @@
+import { useMediaQuery, useTheme } from "@material-ui/core";
+import { ContainerGrid, StyledTitle, ImageBox } from "./style";
+
+export default function DescriptionCard({
+  icon,
+  title,
+  description,
+  isH1,
+  imageBorder,
+}) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <ContainerGrid $isMobile={isMobile} $imageBorder={imageBorder}>
+      <ImageBox $imageBorder={imageBorder} $isMobile={isMobile}>
+        <img src={`/${icon}`} />
+      </ImageBox>
+
+      <StyledTitle>{isH1 ? <h1>{title}</h1> : <h2>{title}</h2>}</StyledTitle>
+
+      <p>{description}</p>
+    </ContainerGrid>
+  );
+}
--- containers/AboutUs/index.js
+++ containers/AboutUs/index.js
@@ -1,7 +1,35 @@
 import { Container, useTheme, useMediaQuery } from "@material-ui/core";
 
 import PageTemplate from "../../components/PageTemplate";
+import DescriptionCard from "../../components/DescriptionCard";
 import ContributorsContainer from "../../components/ContributorsContainer";
+
+const textBlocks = [
+  {
+    icon: "icon-background-1.svg",
+    title: "Our paper",
+    description:
+      "Computer progress is a platform for viewing paper data that\
+              addresses the computational limits of deep learning. Our\
+              inspiration for creating this site was the paper The Computational\
+              Limits of Deep Learning, where one of the group members, Gabriel\
+              F. Manso, is one of the authors of the paper.\
+              The platform was created in conjunction with students and\
+              professors from YONSEI University, who validated our product, and\
+              helped us to improve it. This platform is open source and is open\
+              to contributions from new employees.",
+  },
+  {
+    icon: "icon-background-2.svg",
+    title: "Our mission",
+    description:
+      "Our mission is to make it easier to view paper data that deal with\
+    Deep Learning, and concentrate that knowledge in a platform. In\
+    addition, we also want to engage researchers in the field to put\
+    their papers on our platform and also become contributors and\
+    reviewers of papers to be submitted by other contributors.",
+  },
+];
 
 const contributors = [
   {
@@ -77,43 +105,21 @@ export default function Home() {
         <Container>
           <StyledTitle>About us</StyledTitle>
 
-          <GridContainer $mobile={isMobile}>
-            <img src="/icon-background-1.svg" />
-
-            <h2>Our paper</h2>
-
-            <p>
-              Computer progress is a platform for viewing paper data that
-              addresses the computational limits of deep learning. Our
-              inspiration for creating this site was the paper The Computational
-              Limits of Deep Learning, where one of the group members, Gabriel
-              F. Manso, is one of the authors of the paper.
-              <br />
-              <br />
-              The platform was created in conjunction with students and
-              professors from YONSEI University, who validated our product, and
-              helped us to improve it. This platform is open source and is open
-              to contributions from new employees.
-            </p>
-          </GridContainer>
+          <DescriptionCard
+            icon={textBlocks[0].icon}
+            title={textBlocks[0].title}
+            description={textBlocks[0].description}
+          />
 
           <ButtonContainer>
             <StyledButton>See our paper</StyledButton>
           </ButtonContainer>
 
-          <GridContainer $mobile={isMobile}>
-            <img src="/icon-background-1.svg" />
-
-            <h2>Our mission</h2>
-
-            <p>
-              Our mission is to make it easier to view paper data that deal with
-              Deep Learning, and concentrate that knowledge in a platform. In
-              addition, we also want to engage researchers in the field to put
-              their papers on our platform and also become contributors and
-              reviewers of papers to be submitted by other contributors.
-            </p>
-          </GridContainer>
+          <DescriptionCard
+            icon={textBlocks[1].icon}
+            title={textBlocks[1].title}
+            description={textBlocks[1].description}
+          />
 
           <ContributorsContainer contributors={contributors} />
         </Container>
