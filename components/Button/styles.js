7c861564e6b0e1b90eba924fa065e8890644b564
Guilherme Banci <guibanci@gmail.com>
2021-06-16 22:34:54 -0300

    creating sign in page
    Signed-off-by: Guilherme Banci <guibanci@gmail.com>

--- components/Button/styles.js
+++ components/Button/styles.js
@@ -1,23 +1,22 @@
 import styled from "styled-components";
 import theme from "../../styles/theme";
+import { Button as MaterialButton } from '@material-ui/core';
 
-export const Button = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 8px 32px;
-  border-radius: 100px;
-  border: none;
+export const Button = styled(MaterialButton).attrs(({ variant, primary }) => ({
+  variant: variant || primary ? 'default' : 'contained',
+  disableElevation: true,
+}))`
+  border-radius: 100px !important;
   cursor: pointer;
   background: ${({ primary, background }) =>
-    background || primary ? theme.colors.white : "transparent"};
+    background || primary ? theme.colors.white : "transparent"} !important;
   color: ${({ primary, color }) =>
     color || primary ? theme.colors.black : theme.colors.white};
   ${({cta}) => cta && `
   background: linear-gradient(268.88deg, rgba(255, 255, 255, 0.1) -7.38%, #9E1FFF 104.79%), #6047FF;
   color: white;
   `}
-  transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out !important;
   &:hover {
     opacity: 0.8;
   }
