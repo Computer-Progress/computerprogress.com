cf6ac9d7de86b15c13afd8bb0183c46803054c73
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2021-06-17 14:55:36 -0300

    Improve design

--- containers/Task/styles.js
+++ containers/Task/styles.js
@@ -1,18 +1,30 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 70%;
+  width: 80%;
   margin: 0 auto;
 
   @media (max-width: 600px) {
     width: 85%;
   }
-    h2 {
+
+  h2 {
     font-family: 'Montserrat';
     font-weight: 500;
-    margin: 50px 0;
+    margin: 100px 0 50px 0;
     padding-top: 0;
     flex: 1;
-    text-align: center;
+
+  }
+
+  h4 {
+    font-family: 'Montserrat';
+    font-weight: 10;
+    font-size: 12px;
+  }
+
+  p {
+    font-family: 'Montserrat';
+    font-weight: 500;
   }
 `;
