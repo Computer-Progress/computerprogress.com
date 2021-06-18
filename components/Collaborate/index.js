841a4f18e1aa5af8befd2f0c034aa9538ec04878
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2021-06-18 03:00:52 -0300

    Add color to button

--- components/Collaborate/index.js
+++ components/Collaborate/index.js
@@ -1,4 +1,4 @@
-import { Path, Wrapper, Container, Text, Call } from "./styles.js";
+import { CollaborateButton, Wrapper, Container, Text, Call } from "./styles.js";
 import Circuit from '../Circuit'
 import Button from '../Button'
 
@@ -8,7 +8,7 @@ export default function Collaborate() {
       <Container>
         <Text> Your help can change everything! </Text>
         <Call> Collaborate on the understanding of the hardware <br /> burden influence on machine learning.</Call>
-        <Button link='/collaborate' cta>SEE HOW TO COLLABORATE</Button>
+        <CollaborateButton link='/collaborate'>SEE HOW TO COLLABORATE</CollaborateButton>
       </Container>
     </Wrapper>
   );
