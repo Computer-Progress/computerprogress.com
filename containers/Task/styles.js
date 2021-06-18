9342b3a6d4f63715b305461b1b16891df3c5933c
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2021-06-18 02:52:30 -0300

    Improve task page

--- containers/Task/styles.js
+++ containers/Task/styles.js
@@ -2,7 +2,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 80%;
+  width: 75%;
   margin: 0 auto;
   @media (max-width: 600px) {
     width: 85%;
@@ -21,6 +21,7 @@ export const Container = styled.div`
   }
   p {
     font-family: 'Montserrat';
-    font-weight: 500;
+    font-weight: 300;
+    font-size: 15px;
   }
 `;
