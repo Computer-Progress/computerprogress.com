96d025c53882f67bc28473b679f936a4802679de
Andrew Lucas <andrewlucasgs@gmail.com>
2021-06-18 12:18:33 -0300

    Fix task getProps route

--- pages/tasks/[taskId]/index.js
+++ pages/tasks/[taskId]/index.js
@@ -2,7 +2,7 @@ export { default } from "../../../containers/Task";
 
 export const getServerSideProps = async ({ query }) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/tasks/${query.taskId}`);
+    const res = await fetch(`/api/tasks/${query.taskId}`);
     const benchmarks = await res.json();
 
     return {
