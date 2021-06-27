9829ed36b34c6db9b1b663be0ca07a2a17cf46f7
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-27 11:38:04 -0300

    feat(api): fetch home data from API

--- components/TaskTable/index.js
+++ components/TaskTable/index.js
@@ -1,14 +1,29 @@
+import TaskTableTabs from "../TabsTableTabs";
+
 import { Grid, Card, useMediaQuery } from "@material-ui/core";
+
 import { MuiTheme } from "../../styles/theme";
 import { StyledGridItem } from "./styles";
+import { useEffect, useState } from "react";
 
-export default function Domain() {
+export default function TaskTable({ tasks }) {
   const isMobile = useMediaQuery(MuiTheme.breakpoints.down("sm"));
+  const [tabs, setTabs] = useState([]);
+
+  useEffect(() => {
+    const tabs = tasks.map((task) => ({
+      id: task.task_id,
+      identifier: task.task_identifier,
+      name: task.task_name,
+    }));
+
+    setTabs(tabs);
+  }, []);
 
   return (
     <Grid container spacing={1}>
       <StyledGridItem $order={1}>
-        <Card>Domain tabs</Card>
+        <TaskTableTabs />
       </StyledGridItem>
 
       <StyledGridItem $order={2} sm={6} md={2}>
--- pages/home.js
+++ pages/home.js
@@ -2,14 +2,32 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { MuiTheme } from "../styles/theme";
 
 import DefaultLayout from "../layouts/DefaultLayout";
-import TaskChart from "../components/TaskChart";
+import TaskTable from "../components/TaskTable";
 
-export default function Home() {
+export default function Home({ tasks }) {
   return (
     <ThemeProvider theme={MuiTheme}>
       <DefaultLayout>
-        <TaskChart />
+        <TaskTable tasks={tasks} />
       </DefaultLayout>
     </ThemeProvider>
   );
+}
+
+export async function getServerSideProps() {
+  const URL =
+    "http://ec2-3-129-18-205.us-east-2.compute.amazonaws.com/api/v1/sota";
+
+  try {
+    const response = await fetch(URL);
+    const tasks = await response.json();
+
+    return {
+      props: {
+        tasks,
+      },
+    };
+  } catch {
+    return { props: {} };
+  }
 }
