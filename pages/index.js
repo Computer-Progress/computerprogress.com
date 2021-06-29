a680ef0ff85e8cf861d5e16832900b188fd38b08
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-29 18:33:01 -0300

    feat(page): move /home to /

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,19 @@
-export { default } from '../containers/Home';
+export { default } from "../containers/Home";
+
+export async function getServerSideProps() {
+  const URL =
+    "http://ec2-3-129-18-205.us-east-2.compute.amazonaws.com/api/v1/sota";
+
+  try {
+    const response = await fetch(URL);
+    const tasks = await response.json();
+
+    return {
+      props: {
+        tasks,
+      },
+    };
+  } catch {
+    return { props: {} };
+  }
+}
