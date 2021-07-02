c5502ab71644959d99ec1a51ed4fc0474f6041dc
Irwin Schmitt <irwinschmitt@gmail.com>
2021-07-01 21:02:40 -0300

    fix(collaborate): increase margin between sections

--- containers/Collaborate/index.js
+++ containers/Collaborate/index.js
@@ -8,123 +8,161 @@ import {
   ListItemText,
   Chip,
   Typography,
-  Card,
+  Box,
 } from "@material-ui/core";
 
 import PageTemplate from "../../components/PageTemplate";
 
 import CollaborateImportance from "../../components/CollaborateImportance";
-import CollaborateJoin from "../../components/CollaborateJoin";
 import CollaborateQuestion from "../../components/CollaborateQuestion";
 
-import { StyledBox, StyledTypographyBody1, StyledListIcon } from "./styles";
+import {
+  StyledFlexboxSection,
+  StyledBox,
+  StyledTypographyBody1,
+  StyledListIcon,
+  StyledButton,
+} from "./styles";
 
 export default function Collaborate() {
   return (
     <ThemeProvider theme={MuiTheme}>
       <PageTemplate>
-        <StyledBox>
-          <Typography variant="h1" align="center">
-            Learn how to report computing power
-          </Typography>
-        </StyledBox>
+        {/* Learn how to report computing power */}
+        <StyledFlexboxSection>
+          <Box mb={8}>
+            <Typography variant="h1" align="center">
+              Learn how to report computing power
+            </Typography>
+          </Box>
 
-        <StyledBox>
-          <StyledTypographyBody1>
-            Two are the most usual ways used to report the computational burden
-            of Deep Learning:
-          </StyledTypographyBody1>
-        </StyledBox>
+          <StyledBox>
+            <StyledTypographyBody1>
+              Two are the most usual ways used to report the computational
+              burden of Deep Learning:
+            </StyledTypographyBody1>
+          </StyledBox>
 
-        <StyledBox>
-          <Typography variant="h2">Computation per network pass</Typography>
-        </StyledBox>
+          <StyledBox>
+            <Typography variant="h2">Computation per network pass</Typography>
+          </StyledBox>
 
-        <StyledBox>
-          <StyledTypographyBody1>
-            The number of floating-point operations required for a single pass
-            (i.e. weight adjustment), also measurable using multiply-adds, in a
-            given deep learning model.
-          </StyledTypographyBody1>
-        </StyledBox>
+          <StyledBox>
+            <StyledTypographyBody1>
+              The number of floating-point operations required for a single pass
+              (i.e. weight adjustment), also measurable using multiply-adds, in
+              a given deep learning model.
+            </StyledTypographyBody1>
+          </StyledBox>
 
-        <StyledBox>
-          <StyledTypographyBody1>
-            To report this data, you can use either:
-          </StyledTypographyBody1>
+          <StyledBox>
+            <StyledTypographyBody1>
+              To report this data, you can use either:
+            </StyledTypographyBody1>
 
-          <List>
-            <ListItem>
-              <ListItemIcon>
-                <StyledListIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary={
-                  <>
-                    <Chip label="Gigaflops" /> or <Chip label="Multiply-adds" />
-                  </>
-                }
-              />
-            </ListItem>
-          </List>
-        </StyledBox>
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <StyledListIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <>
+                      <Chip label="Gigaflops" /> or{" "}
+                      <Chip label="Multiply-adds" />
+                    </>
+                  }
+                />
+              </ListItem>
+            </List>
+          </StyledBox>
 
-        <StyledBox>
-          <Typography variant="h2">Hardware burden</Typography>
-        </StyledBox>
+          <StyledBox>
+            <Typography variant="h2">Hardware burden</Typography>
+          </StyledBox>
 
-        <StyledBox>
-          <StyledTypographyBody1>
-            The computational capability of the hardware used to train the
-            model, calculated as the number of processors multiplied by the
-            computation rate and time.
-          </StyledTypographyBody1>
-        </StyledBox>
+          <StyledBox>
+            <StyledTypographyBody1>
+              The computational capability of the hardware used to train the
+              model, calculated as the number of processors multiplied by the
+              computation rate and time.
+            </StyledTypographyBody1>
+          </StyledBox>
 
-        <StyledBox>
-          <StyledTypographyBody1>
-            To report this data, you will need:
-          </StyledTypographyBody1>
+          <StyledBox>
+            <StyledTypographyBody1>
+              To report this data, you will need:
+            </StyledTypographyBody1>
 
-          <List>
-            <ListItem>
-              <ListItemIcon>
-                <StyledListIcon />
-              </ListItemIcon>
-              <ListItemText primary={<Chip label="TPU, GPU and CPU model" />} />
-            </ListItem>
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <StyledListIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={<Chip label="TPU, GPU and CPU model" />}
+                />
+              </ListItem>
 
-            <ListItem>
-              <ListItemIcon>
-                <StyledListIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary={<Chip label="Number of TPUs, GPUs and CPUs" />}
-              />
-            </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <StyledListIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={<Chip label="Number of TPUs, GPUs and CPUs" />}
+                />
+              </ListItem>
 
-            <ListItem>
-              <ListItemIcon>
-                <StyledListIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary={<Chip label="Training time in seconds" />}
-              />
-            </ListItem>
-          </List>
-        </StyledBox>
+              <ListItem>
+                <ListItemIcon>
+                  <StyledListIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={<Chip label="Training time in seconds" />}
+                />
+              </ListItem>
+            </List>
+          </StyledBox>
+        </StyledFlexboxSection>
 
-        <StyledBox>
-          <CollaborateImportance />
-        </StyledBox>
+        {/* People talking about the importance of computing power */}
+        <StyledFlexboxSection>
+          <Box mb={8}>
+            <Typography variant="h1" align="center">
+              People talking about the importance of computing power
+            </Typography>
+          </Box>
 
-        <StyledBox>
-          <CollaborateJoin />
-        </StyledBox>
+          <StyledBox>
+            <CollaborateImportance />
+          </StyledBox>
+        </StyledFlexboxSection>
 
-        <StyledBox>
+        {/* Join us! */}
+        <StyledFlexboxSection>
+          {/* <CollaborateJoin /> */}
+
+          <StyledBox>
+            <Typography variant="h2" align="center">
+              Join us!
+            </Typography>
+          </StyledBox>
+
+          <StyledBox>
+            <Typography variant="h3" align="center">
+              Join our community and start contributing to the future of
+              artificial intelligence right now!
+            </Typography>
+          </StyledBox>
+
+          <StyledBox display="flex" justifyContent="center">
+            <StyledButton>Submit paper</StyledButton>
+          </StyledBox>
+        </StyledFlexboxSection>
+
+        <StyledFlexboxSection>
           <CollaborateQuestion />
-        </StyledBox>
+        </StyledFlexboxSection>
       </PageTemplate>
     </ThemeProvider>
   );
--- containers/Collaborate/styles.js
+++ containers/Collaborate/styles.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
-import { Box, ListItem, Typography } from "@material-ui/core";
+import { Box, Button, ListItem, Typography } from "@material-ui/core";
+
+export const StyledFlexboxSection = styled(Box).attrs({
+  my: 5,
+})``;
 
 export const StyledBox = styled(Box).attrs({
-  my: 2,
+  mt: 5,
 })``;
 
 export const StyledTypographyBody1 = styled(Typography).attrs({
@@ -16,4 +20,22 @@ export const StyledListIcon = styled.div`
   border: solid 4px #7100c9;
   border-radius: 50%;
   background-color: #ffffff;
+`;
+
+export const StyledButton = styled(Button).attrs({
+  color: "primary",
+  variant: "contained",
+})`
+  padding-right: 50px !important;
+  padding-left: 50px !important;
+
+  border-radius: 100px !important;
+
+  color: white !important;
+  background: linear-gradient(
+      268.88deg,
+      rgba(255, 255, 255, 0.1) -7.38%,
+      #7100c9 104.79%
+    ),
+    #2000e5 !important;
 `;
