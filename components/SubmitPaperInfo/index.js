0c57ef8444296759e4ff150d9b405301eb80d30f
Irwin Schmitt <irwinschmitt@gmail.com>
2021-07-28 01:08:21 -0300

    Add date picker to paper info

--- components/SubmitPaperInfo/index.js
+++ components/SubmitPaperInfo/index.js
@@ -12,6 +12,12 @@ import {
   InputAdornment,
 } from "@material-ui/core";
 
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+
 import PersonAddOutlinedIcon from "@material-ui/icons/PersonAddOutlined";
 import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from "@material-ui/icons/Close";
@@ -23,12 +29,13 @@ import { StyledCard, StyledBoxContainer, StyledTextField } from "./styles";
 export default function SubmitPaperInfo() {
   const theme = useTheme();
   const isLargeScreen = useMediaQuery(theme.breakpoints.up("md"));
+  // const [selectedDate, handleDateChange] = useState(new Date());
 
   const [paperInfo, setPaperInfo] = useState({
     title: "",
     link: "",
     codeLink: "",
-    releaseDate: "",
+    releaseDate: null,
   });
   const [newAuthor, setNewAuthor] = useState("");
   const [authorsList, setAuthorsList] = useState([]);
@@ -55,135 +62,151 @@ export default function SubmitPaperInfo() {
   }
 
   return (
-    <StyledCard>
-      <StyledBoxContainer>
-        <Grid container spacing={3}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            container
-            spacing={3}
-            alignContent="flex-start"
-          >
-            <Grid item xs={12}>
-              <Typography variant="h3">Paper information</Typography>
-            </Grid>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <StyledCard>
+        <StyledBoxContainer>
+          <Grid container spacing={3}>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              container
+              spacing={3}
+              alignContent="flex-start"
+            >
+              <Grid item xs={12}>
+                <Typography variant="h3">Paper information</Typography>
+              </Grid>
 
-            <Grid item xs={12}>
-              <Divider />
-            </Grid>
+              <Grid item xs={12}>
+                <Divider />
+              </Grid>
 
-            <Grid item xs={12}>
-              <StyledTextField
-                name="title"
-                label="Title"
-                value={paperInfo.title}
-                variant="outlined"
-                onChange={handlePaperInfoChange}
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <StyledTextField
-                name="link"
-                label="Link"
-                value={paperInfo.link}
-                variant="outlined"
-                onChange={handlePaperInfoChange}
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <StyledTextField
-                name="codeLink"
-                label="Code link"
-                value={paperInfo.codeLink}
-                variant="outlined"
-                onChange={handlePaperInfoChange}
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <StyledTextField
-                name="releaseDate"
-                label="Release date"
-                value={paperInfo.releaseDate}
-                variant="outlined"
-                onChange={handlePaperInfoChange}
-              />
-            </Grid>
-          </Grid>
-          {isLargeScreen && (
-            <Grid item>
-              <MuiDivider orientation="vertical" />
-            </Grid>
-          )}
-
-          <Grid
-            item
-            xs={12}
-            md={6}
-            container
-            spacing={3}
-            alignContent="flex-start"
-          >
-            <Grid item xs={12}>
-              <Typography variant="h3">Authors</Typography>
-            </Grid>
-
-            <Grid item xs={12}>
-              <Divider />
-            </Grid>
-
-            <Grid item xs={12}>
-              <form onSubmit={addAuthor}>
-                <TextField
-                  placeholder="Author name"
-                  margin="normal"
-                  fullWidth
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <PersonAddOutlinedIcon size="small" />
-                      </InputAdornment>
-                    ),
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton size="small">
-                          <AddIcon />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                  onChange={(event) => setNewAuthor(event.target.value)}
+              <Grid item xs={12}>
+                <StyledTextField
+                  name="title"
+                  label="Title"
+                  value={paperInfo.title}
+                  variant="outlined"
+                  onChange={handlePaperInfoChange}
                 />
-              </form>
+              </Grid>
 
-              {authorsList.map((author, index) => (
-                <Grid item key={index}>
-                  <Box display="flex">
-                    <Box my={1} display="inline-block" flexGrow={1}>
-                      <Typography variant="body2">{author}</Typography>
+              <Grid item xs={6}>
+                <StyledTextField
+                  name="link"
+                  label="Link"
+                  value={paperInfo.link}
+                  variant="outlined"
+                  onChange={handlePaperInfoChange}
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <StyledTextField
+                  name="codeLink"
+                  label="Code link"
+                  value={paperInfo.codeLink}
+                  variant="outlined"
+                  onChange={handlePaperInfoChange}
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                {/* <StyledTextField
+                  name="releaseDate"
+                  label="Release date"
+                  value={paperInfo.releaseDate}
+                  variant="outlined"
+                  onChange={handlePaperInfoChange}
+                /> */}
+
+                <KeyboardDatePicker
+                  name="releaseDate"
+                  autoOk
+                  variant="inline"
+                  inputVariant="outlined"
+                  label="Release date"
+                  format="MM/dd/yyyy"
+                  value={paperInfo.releaseDate}
+                  InputAdornmentProps={{ position: "end" }}
+                  onChange={(date) =>
+                    setPaperInfo({ ...paperInfo, releaseDate: date })
+                  }
+                />
+              </Grid>
+            </Grid>
+            {isLargeScreen && (
+              <Grid item>
+                <MuiDivider orientation="vertical" />
+              </Grid>
+            )}
+
+            <Grid
+              item
+              xs={12}
+              md={6}
+              container
+              spacing={3}
+              alignContent="flex-start"
+            >
+              <Grid item xs={12}>
+                <Typography variant="h3">Authors</Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Divider />
+              </Grid>
+
+              <Grid item xs={12}>
+                <form onSubmit={addAuthor}>
+                  <TextField
+                    placeholder="Author name"
+                    margin="normal"
+                    fullWidth
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <PersonAddOutlinedIcon size="small" />
+                        </InputAdornment>
+                      ),
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton size="small">
+                            <AddIcon />
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                    onChange={(event) => setNewAuthor(event.target.value)}
+                  />
+                </form>
+
+                {authorsList.map((author, index) => (
+                  <Grid item key={index}>
+                    <Box display="flex">
+                      <Box my={1} display="inline-block" flexGrow={1}>
+                        <Typography variant="body2">{author}</Typography>
+                      </Box>
+
+                      <Box display="inline-block">
+                        <IconButton
+                          size="small"
+                          onClick={() => removeAuthor(index)}
+                        >
+                          <CloseIcon />
+                        </IconButton>
+                      </Box>
                     </Box>
 
-                    <Box display="inline-block">
-                      <IconButton
-                        size="small"
-                        onClick={() => removeAuthor(index)}
-                      >
-                        <CloseIcon />
-                      </IconButton>
-                    </Box>
-                  </Box>
-
-                  <MuiDivider />
-                </Grid>
-              ))}
+                    <MuiDivider />
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </StyledBoxContainer>
-    </StyledCard>
+        </StyledBoxContainer>
+      </StyledCard>
+    </MuiPickersUtilsProvider>
   );
 }
