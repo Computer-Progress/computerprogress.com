4650be59b38fe3a1bf4ed5c8a32a8b0dc7146909
Irwin Schmitt <irwinschmitt@gmail.com>
2021-08-05 02:31:48 -0300

    Create profile page inputs

--- containers/Profile/index.js
+++ containers/Profile/index.js
@@ -1,21 +1,86 @@
-
 import UserPageTemplate from "../../components/UserPageTemplate";
 import { MuiTheme } from "../../styles/theme";
 import { Title } from "./styles";
-import { useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { Box, Button, Divider, Grid, TextField } from "@material-ui/core";
 
 export default function Review() {
   // const isMobile = useMediaQuery(MuiTheme.breakpoints.down("md"));
-  const userState = useSelector(state => state.UserReducer);
+  const userState = useSelector((state) => state.UserReducer);
 
   useEffect(() => {
-    console.log(userState)
-  }, [userState])
+    console.log(userState);
+  }, [userState]);
 
   return (
     <UserPageTemplate selectedPage={0}>
       <Title>Profile</Title>
+
+      <Grid container>
+        <Grid item xs={6} container spacing={3}>
+          <Grid item xs={6}>
+            <TextField label="First name" variant="outlined" fullWidth />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField label="Last name" variant="outlined" fullWidth />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField label="Email" variant="outlined" fullWidth />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              style={{ borderRadius: "100px" }}
+              color="primary"
+              variant="contained"
+              disableElevation
+            >
+              <Box px={3}>Update profile</Box>
+            </Button>
+          </Grid>
+
+          <Divider />
+
+          <Grid item xs={6}>
+            <TextField
+              label="Current password"
+              variant="outlined"
+              fullWidth
+              type="password"
+            />
+          </Grid>
+
+          <Grid item xs={6}>
+            <TextField
+              label="New password"
+              variant="outlined"
+              fullWidth
+              type="password"
+            />
+          </Grid>
+
+          <Grid item xs={6}>
+            <TextField
+              label="Confirm new password"
+              variant="outlined"
+              fullWidth
+              type="password"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              style={{ borderRadius: "100px" }}
+              color="primary"
+              variant="contained"
+              disableElevation
+            >
+              <Box px={3}>Update password</Box>
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </UserPageTemplate>
   );
 }
--- pages/profile.js
+++ pages/profile.js
@@ -1,1 +1,21 @@
 export { default } from "../containers/Profile";
+import useApi from "../services/useApi";
+
+export const getServerSideProps = async ({ query }) => {
+  const api = useApi(true);
+  try {
+    const res = await api.get(`/users/me`);
+    const profile = res.data;
+    console.log(profile);
+
+    return {
+      props: {
+        profile,
+      },
+    };
+  } catch {
+    return {
+      props: {},
+    };
+  }
+};
