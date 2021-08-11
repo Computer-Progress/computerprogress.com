84591d065315b2f0ded3e7b70a50e3336719005f
Irwin Schmitt <irwinschmitt@gmail.com>
2021-08-11 18:59:00 -0300

    Save user new data

--- containers/Profile/index.js
+++ containers/Profile/index.js
@@ -1,7 +1,5 @@
 import UserPageTemplate from "../../components/UserPageTemplate";
-import { MuiTheme } from "../../styles/theme";
-import { Title } from "./styles";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import {
   Box,
@@ -11,104 +9,176 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
+import useApi from "../../services/useApi";
+import { Creators as alertActions } from "../../store/ducks/alert";
+import axios from "axios";
 
 export default function Profile() {
   // const isMobile = useMediaQuery(MuiTheme.breakpoints.down("md"));
+  const api = useApi();
+  const dispatch = useDispatch();
+
+  const [profile, setProfile] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const [profileChanged, setProfileChanged] = useState(false);
+  const [passwordChanged, setPasswordChanged] = useState(false);
+
   const userState = useSelector((state) => state.UserReducer);
 
-  console.log(userState);
+  useEffect(() => {
+    setProfile({
+      firstName: userState.first_name ?? "",
+      lastName: userState.last_name ?? "",
+      email: userState.email ?? "",
+    });
+
+    setProfileChanged(false);
+  }, [userState]);
 
   useEffect(() => {
-    console.log(userState);
-  }, [userState]);
+    if (profile.firstName && profile.firstName !== userState.first_name) {
+      setProfileChanged(true);
+    } else if (profile.lastName && profile.lastName !== userState.lastName) {
+      setProfileChanged(true);
+    } else if (profile.email && profile.email !== userState.email) {
+      setProfileChanged(true);
+    } else {
+      setProfileChanged(false);
+    }
+  }, [profile]);
+
+  function handleProfileChange(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+
+    setProfile({ ...profile, [key]: value });
+  }
+
+  async function updateProfile() {
+    const body = {
+      first_name: profile.firstName,
+      last_name: profile.lastName,
+      email: profile.email,
+    };
+
+    const response = await api
+      .put("/users/me", body)
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          // Save new data
+          // dispatch(...)
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   return (
     <UserPageTemplate selectedPage={0}>
-      <Grid container display="flex">
+      <Grid container spacing={3}>
         <Grid item xs={12}>
           <Box py={3}>
             <Typography variant="h2">Profile</Typography>
           </Box>
         </Grid>
 
-        <Grid item xs={6} container spacing={3}>
-          <Grid item xs={6}>
-            <TextField
-              label="First name"
-              value={userState.first_name}
-              variant="outlined"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Last name"
-              value={userState.last_name}
-              variant="outlined"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={7}>
-            <TextField
-              label="Email"
-              value={userState.email}
-              variant="outlined"
-              fullWidth
-            />
-          </Grid>
+        <Grid item xs={4}>
+          <TextField
+            value={profile.firstName}
+            name="firstName"
+            label="First name"
+            variant="outlined"
+            fullWidth
+            onChange={handleProfileChange}
+          />
+        </Grid>
 
-          <Grid item xs={12}>
-            <Button
-              disabled
-              style={{ borderRadius: "100px" }}
-              color="primary"
-              variant="contained"
-              disableElevation
-            >
-              <Box px={3}>Update profile</Box>
-            </Button>
-          </Grid>
+        <Grid item />
 
-          <Divider />
+        <Grid item xs={4}>
+          <TextField
+            value={profile.lastName}
+            name="lastName"
+            label="Last name"
+            variant="outlined"
+            fullWidth
+            onChange={handleProfileChange}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField
+            value={profile.email}
+            name="email"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            onChange={handleProfileChange}
+          />
+        </Grid>
 
-          <Grid item xs={7}>
-            <TextField
-              label="Current password"
-              variant="outlined"
-              fullWidth
-              type="password"
-            />
-          </Grid>
+        <Grid item xs={12}>
+          <Button
+            disabled={!profileChanged}
+            style={{ borderRadius: "100px" }}
+            color="primary"
+            variant="contained"
+            disableElevation
+            onClick={updateProfile}
+          >
+            <Box px={3}>Update profile</Box>
+          </Button>
+        </Grid>
 
-          <Grid item xs={7}>
-            <TextField
-              label="New password"
-              variant="outlined"
-              fullWidth
-              type="password"
-            />
-          </Grid>
+        <Divider />
 
-          <Grid item xs={7}>
-            <TextField
-              label="Confirm new password"
-              variant="outlined"
-              fullWidth
-              type="password"
-            />
-          </Grid>
+        <Grid item xs={4}>
+          <TextField
+            label="Current password"
+            variant="outlined"
+            fullWidth
+            type="password"
+          />
+        </Grid>
 
-          <Grid item xs={12}>
-            <Button
-              disabled
-              style={{ borderRadius: "100px" }}
-              color="primary"
-              variant="contained"
-              disableElevation
-            >
-              <Box px={3}>Update password</Box>
-            </Button>
-          </Grid>
+        <Grid item xs={8} />
+
+        <Grid item xs={4}>
+          <TextField
+            label="New password"
+            variant="outlined"
+            fullWidth
+            type="password"
+          />
+        </Grid>
+
+        <Grid item xs={8} />
+
+        <Grid item xs={4}>
+          <TextField
+            label="Confirm new password"
+            variant="outlined"
+            fullWidth
+            type="password"
+          />
+        </Grid>
+
+        <Grid item xs={8} />
+
+        <Grid item xs={12}>
+          <Button
+            disabled
+            style={{ borderRadius: "100px" }}
+            color="primary"
+            variant="contained"
+            disableElevation
+          >
+            <Box px={3}>Update password</Box>
+          </Button>
         </Grid>
       </Grid>
     </UserPageTemplate>
