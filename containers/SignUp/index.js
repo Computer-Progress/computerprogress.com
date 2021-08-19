0af5b841fb440a0c80c85dcf1f2947dbb40e3b75
Irwin Schmitt <irwinschmitt@gmail.com>
2021-08-19 20:04:49 -0300

    Add sign up logic

--- containers/SignUp/index.js
+++ containers/SignUp/index.js
@@ -12,8 +12,72 @@ import {
   SignButton,
   Divider,
 } from "./styles";
+import { useEffect, useState } from "react";
+import useApi from "../../services/useApi";
+import { Creators as alertActions } from "../../store/ducks/alert";
+import { useDispatch } from "react-redux";
+import router from "next/router";
 
 export default function SignUp() {
+  const api = useApi();
+  const dispatch = useDispatch();
+
+  const emptyAccount = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  };
+
+  const [account, setAccount] = useState(emptyAccount);
+
+  const [isAccountValid, setIsAccountValid] = useState(false);
+  const [isSubmissionLoading, setIsSubmissionLoading] = useState(false);
+
+  useEffect(() => {
+    const isSomeInputEmpty = Object.values(account).some(
+      (input) => input.length === 0
+    );
+
+    setIsAccountValid(!isSomeInputEmpty);
+  }, [account]);
+
+  function handleChange(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+
+    setAccount({ ...account, [key]: value });
+  }
+
+  async function submitSignIn() {
+    setIsSubmissionLoading(true);
+
+    try {
+      const response = await api.post("/users/open", account);
+
+      dispatch(
+        alertActions.openAlert({
+          open: true,
+          message: `An email confirmation was sent to ${account.email}.`,
+          type: "info",
+        })
+      );
+
+      setIsSubmissionLoading(true);
+      setAccount(emptyAccount);
+      router.push("/signin");
+    } catch (error) {
+      dispatch(
+        alertActions.openAlert({
+          open: true,
+          message: error.message,
+          type: "error",
+        })
+      );
+    }
+    setIsSubmissionLoading(false);
+  }
   return (
     <PageTemplate>
       <Container>
@@ -26,15 +90,49 @@ export default function SignUp() {
         </InfoContainer>
         <StyledBox>
           <h2>Sign Up</h2>
-          <Input label="Email" />
-          <Input label="Name" />
-          <Input label="Last Name" />
-          <Input label="Password" />
-          <Input label="Confirm Password" />
+          <Input
+            name="first_name"
+            label="First name"
+            value={account["first_name"]}
+            onChange={handleChange}
+          />
+          <Input
+            name="last_name"
+            label="Last name"
+            value={account["last_name"]}
+            onChange={handleChange}
+          />
+          <Input
+            name="email"
+            label="Email"
+            value={account["email"]}
+            onChange={handleChange}
+          />
+          <Input
+            name="password"
+            label="Password"
+            type="password"
+            value={account["password"]}
+            onChange={handleChange}
+          />
+          <Input
+            name="passwordConfirmation"
+            label="Confirm password"
+            type="password"
+            value={account["passwordConfirmation"]}
+            onChange={handleChange}
+          />
           <Question>
             Use 8 or more characters with a mix of letters, numbers & simbols
           </Question>
-          <SignButton>SIGN UP</SignButton>
+          <NewButton
+            color="primary"
+            disabled={!isAccountValid}
+            loading={isSubmissionLoading}
+            onClick={submitSignIn}
+          >
+            Sign in
+          </NewButton>
 
           <Question margin>
             By signing up, you agree to our Data Policy
