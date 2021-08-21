e960f23ab86f703e83453864b6cb325e84c431eb
Irwin Schmitt <irwinschmitt@gmail.com>
2021-08-21 15:12:24 -0300

    Fix next.js element height

--- components/PageTemplate/styles.js
+++ components/PageTemplate/styles.js
@@ -4,6 +4,7 @@ import { Container } from "@material-ui/core";
 export const StyledContainer = styled(Container).attrs({
   // maxWidth: "xl",
 })`
+  min-height: 100% !important;
   margin: 1rem 0;
   max-width: 1500px !important;
   display: flex !important;
@@ -13,7 +14,7 @@ export const StyledContainer = styled(Container).attrs({
   h1 {
     font-weight: normal;
   }
-  
+
   @media only screen and (max-width: 1450px) {
     margin: 0px 0px !important;
   }
