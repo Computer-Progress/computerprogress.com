0d29045586dd5a0c285fc1b76211d584570cb26f
Fellipe <araujo.fellipe@hotmail.com>
2021-08-25 12:37:56 -0300

    Created breakpoint to tablet device and added conditional to set border radius value.

--- components/TaskTableChart/index.js
+++ components/TaskTableChart/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { CircularProgress, Box } from "@material-ui/core";
+import { CircularProgress, useMediaQuery } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 
 import { StyledCard, StyledBox } from "./styles";
 import Chart from "../Chart";
@@ -9,8 +10,12 @@ export default function TaskTableChart({ isLoading, data, computingPower, label,
   //   name: 'Hardware Burden',
   //   value: 'hardware_burden',
   // }
+
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
-    <StyledCard>
+    <StyledCard style={{ borderRadius: isTablet ? "16px" : "0 16px 16px 0" }}>
       {isLoading ? (
         <StyledBox>
           <CircularProgress />
