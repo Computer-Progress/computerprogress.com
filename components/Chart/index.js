bef3ddf5d01519518441d5ba64d92408cf802aa4
Andrew Lucas <andrewlucasgs@gmail.com>
2021-09-27 20:19:26 -0300

    Fix graph credits links

--- components/Chart/index.js
+++ components/Chart/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ChartWrapper } from './styles'
+import { ChartWrapper } from "./styles";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 import HighchartsExporting from "highcharts/modules/exporting";
@@ -7,14 +7,13 @@ import regression from "regression";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { MuiTheme } from "../../styles/theme";
 
-
 const chart = ({ data, label, isByYear, computingPower }) => {
   const isMobile = useMediaQuery(MuiTheme.breakpoints.down("md"));
   const [chartOptions, setChartOptions] = useState({
     title: {
       text: "Loading...",
-    }
-  })
+    },
+  });
   const generateChart = (list, label) => {
     let data_points = [];
     let info_points = [];
@@ -23,8 +22,10 @@ const chart = ({ data, label, isByYear, computingPower }) => {
       const element = list[index];
       if (element[label] && element[computingPower.value]) {
         let x, y;
-        x = isByYear ? new Date(element.paper_publication_date).getFullYear() : Math.log10(element[computingPower.value]);
-        y = Math.log10(1 / (1 - (element[label] / 100)));
+        x = isByYear
+          ? new Date(element.paper_publication_date).getFullYear()
+          : Math.log10(element[computingPower.value]);
+        y = Math.log10(1 / (1 - element[label] / 100));
         const point = [x, y];
         data_points.push(point);
 
@@ -39,10 +40,10 @@ const chart = ({ data, label, isByYear, computingPower }) => {
             radius: 4,
             states: {
               hover: {
-                enabled: true
-              }
-            }
-          }
+                enabled: true,
+              },
+            },
+          },
         };
         info_points.push(info);
       }
@@ -56,23 +57,27 @@ const chart = ({ data, label, isByYear, computingPower }) => {
         scatter: {
           dataLabels: {
             enabled: false,
-            format: "{series.name}"
+            format: "{series.name}",
           },
           enableMouseTracking: true,
           color: "#073b4c",
           tooltip: {
             headerFormat: "<b>{series.name}</b><br>",
             pointFormatter: function () {
-              let y = (1 - 10**(-this.y)) * 100;
+              let y = (1 - 10 ** -this.y) * 100;
               y = Math.round(y * 100) / 100;
               let x = Math.round(this.x * 100) / 100;
-              return `${label}: ${y}% - ${isByYear ? `Year: ${x}` : `Computation: 10e${x < 0 ? '' : '+'}${x.toFixed(1)}`} `;
-            }
-          }
+              return `${label}: ${y}% - ${
+                isByYear
+                  ? `Year: ${x}`
+                  : `Computation: 10e${x < 0 ? "" : "+"}${x.toFixed(1)}`
+              } `;
+            },
+          },
         },
         line: {
-          color: "#000000"
-        }
+          color: "#000000",
+        },
       },
 
       series: [
@@ -81,18 +86,27 @@ const chart = ({ data, label, isByYear, computingPower }) => {
           type: "line",
           showInLegend: true,
           color: "#000000",
-          name: isByYear ? `${label} = 1 - 10<span dy="-7"  style="font-size: 10px">${-result.equation[0]} &times Year ${result.equation[1]}</span><span dy="7">` : `${label} = 1 - 10<span dy="-7"  style="font-size: 10px">${-result.equation[1]}</span><span dy="7"> &times; (${computingPower.name})</span><span dy="-7" style="font-size: 10px">${-result.equation[0]}</span>`,
-          data: [result.points[0], result.points[result.points.length - 1]], 
+          name: isByYear
+            ? `${label} = 1 - 10<span dy="-7"  style="font-size: 10px">${-result
+                .equation[0]} &times Year ${
+                result.equation[1]
+              }</span><span dy="7">`
+            : `${label} = 1 - 10<span dy="-7"  style="font-size: 10px">${-result
+                .equation[1]}</span><span dy="7"> &times; (${
+                computingPower.name
+              })</span><span dy="-7" style="font-size: 10px">${-result
+                .equation[0]}</span>`,
+          data: [result.points[0], result.points[result.points.length - 1]],
           marker: {
-            enabled: false
+            enabled: false,
           },
           states: {
             hover: {
-              lineWidth: 3
-            }
+              lineWidth: 3,
+            },
           },
-          enableMouseTracking: false
-        }
+          enableMouseTracking: false,
+        },
       ],
       exporting: {
         width: 2000,
@@ -102,46 +116,65 @@ const chart = ({ data, label, isByYear, computingPower }) => {
         align: "center",
         verticalAlign: "top",
         fontSize: isMobile ? "8px" : "18px",
-        symbolHeight: .001,
-        symbolWidth: .001,
-        symbolRadius: .001,
+        symbolHeight: 0.001,
+        symbolWidth: 0.001,
+        symbolRadius: 0.001,
         fontFamily: "Montserrat, sans-serif",
       },
+      subtitle: {
+        // align: 'center',
+        verticalAlign: "bottom",
+        style: {
+          fontSize: "10px",
+        },
+        x: 0,
+        y: 10,
+        useHTML: true,
+        text:
+          '<a target="_blank" href="https://arxiv.org/abs/2007.05558">' +
+          "Ⓒ The Computational Limits of Deep Learning, N.C. THOMPSON, K. GREENEWALD, K. LEE, G.F. MANSO</a>" +
+          '<a target="_blank" href="https://dblp.uni-trier.de/rec/journals/corr/abs-2007-05558.html?view=bibtex">' +
+          "  [CITE]</a>",
+      },
       credits: {
-        text: '<a href="https://arxiv.org/abs/2007.05558">' +
-              'Ⓒ The Computational Limits of Deep Learning, N.C. THOMPSON, K. GREENEWALD, K. LEE, G.F. MANSO</a>' + 
-              '<a href="https://dblp.uni-trier.de/rec/journals/corr/abs-2007-05558.html?view=bibtex">' +
-              '  [CITE]</a>',
+        enabled: false,
+        text:
+          '<a target="_blank" href="https://arxiv.org/abs/2007.05558">' +
+          "Ⓒ The Computational Limits of Deep Learning, N.C. THOMPSON, K. GREENEWALD, K. LEE, G.F. MANSO</a>" +
+          '<a target="_blank" href="https://dblp.uni-trier.de/rec/journals/corr/abs-2007-05558.html?view=bibtex">' +
+          "  [CITE]</a>",
         position: {
-          align: 'center',
+          align: "center",
           y: -2,
-          x: 50
-      }
+          x: 50,
+        },
       },
       title: {
         text: "",
       },
       xAxis: {
         title: {
-          text: isByYear ? 'Year' : `Computation (${computingPower.name})`,
+          text: isByYear ? "Year" : `Computation (${computingPower.name})`,
           margin: 20,
           style: {
             color: "#333",
             fontWeight: "bold",
             fontSize: isMobile ? "10px" : "14px",
-            fontFamily: "Montserrat, sans-serif"
-          }
+            fontFamily: "Montserrat, sans-serif",
+          },
         },
         tickInterval: 1,
         labels: {
           style: {
             fontSize: 15,
-            fontFamily: "Montserrat, sans-serif"
+            fontFamily: "Montserrat, sans-serif",
           },
           formatter: function () {
-            return isByYear ? this.value : `10e${parseFloat(this.value) < 0 ? '' : '+'}` + this.value;
-          }
-        }
+            return isByYear
+              ? this.value
+              : `10e${parseFloat(this.value) < 0 ? "" : "+"}` + this.value;
+          },
+        },
       },
       yAxis: {
         title: {
@@ -151,28 +184,27 @@ const chart = ({ data, label, isByYear, computingPower }) => {
             color: "#333",
             fontWeight: "bold",
             fontSize: isMobile ? "10px" : "14px",
-            fontFamily: "Montserrat, sans-serif"
-          }
+            fontFamily: "Montserrat, sans-serif",
+          },
         },
         labels: {
           style: {
             fontSize: 15,
-            fontFamily: "Montserrat, sans-serif"
+            fontFamily: "Montserrat, sans-serif",
           },
           formatter: function () {
-            let label = (1 - 10**(-this.value)) * 100;
+            let label = (1 - 10 ** -this.value) * 100;
             return `${this.value ? label.toFixed(1) : 0}%`;
-          }
-        }
-      }
+          },
+        },
+      },
     };
     setChartOptions(chart);
   };
 
   useEffect(() => {
     generateChart(data, label);
-  }, [data, label, isByYear, computingPower])
-
+  }, [data, label, isByYear, computingPower]);
 
   if (typeof Highcharts === "object") {
     HighchartsExporting(Highcharts);
@@ -180,13 +212,8 @@ const chart = ({ data, label, isByYear, computingPower }) => {
 
   return (
     <ChartWrapper>
-
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={chartOptions}
-      />
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
     </ChartWrapper>
   );
-}
+};
 export default chart;
-
