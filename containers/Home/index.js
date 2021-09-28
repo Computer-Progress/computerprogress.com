330985ab6e93894de1c6dd8db0ce1203ec1de5a5
Irwin Schmitt <ischmitt@nelogica.com.br>
2021-09-28 12:51:42 -0300

    Fix collaborate invite height

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -2,23 +2,21 @@ import PageTemplate from "../../components/PageTemplate";
 import { MuiTheme } from "../../styles/theme";
 import TaskTable from "../../components/TaskTable";
 import CollaborateInvite from "../../components/CollaborateInvite";
-import { Box, useMediaQuery, Typography} from "@material-ui/core";
-import { Footnote } from "./styles.js";
-
+import { Box, useMediaQuery } from "@material-ui/core";
 
 export default function Home({ tasks }) {
   const isMobile = useMediaQuery(MuiTheme.breakpoints.down("md"));
 
   return (
-      <PageTemplate isHome>
-        <Box display="flex" flexDirection="column">
-          <Box my={3}>
-            <TaskTable tasks={tasks} />
-          </Box>
-          <Box my={3} height="30vh" width={isMobile ? "100%" : "80%"}>
-            <CollaborateInvite />
-          </Box>
+    <PageTemplate isHome>
+      <Box display="flex" flexDirection="column">
+        <Box my={3}>
+          <TaskTable tasks={tasks} />
         </Box>
-      </PageTemplate>
+        <Box my={3} py={5} width={isMobile ? "100%" : "80%"}>
+          <CollaborateInvite />
+        </Box>
+      </Box>
+    </PageTemplate>
   );
 }
