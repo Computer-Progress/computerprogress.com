4a32636c2a1c00d4a4733434682647ac3798501f
Andrew Lucas <andrewlucasgs@gmail.com>
2022-07-06 17:47:29 -0300

    add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GOOGLE_SHEETS_CLIENT_EMAIL: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    GOOGLE_SHEETS_PRIVATE_KEY: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+  }
 }
 
 module.exports = nextConfig
--- pages/benchmarks/[slug].js
+++ pages/benchmarks/[slug].js
@@ -39,7 +39,7 @@ export async function getStaticProps({ params }) {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every second
-    // revalidate: 60, // In seconds
+    revalidate: 60, // In seconds
   };
 }
 
